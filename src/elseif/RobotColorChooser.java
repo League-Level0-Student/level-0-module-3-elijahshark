06229ab8047e16381730fddc2771b316b1682a5f
elijahshark <league@WTS-iMac-11.attlocal.net>
2018-03-28 17:01:47 -0700

    RobotColorChooser (IP)

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -12,26 +12,38 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
 		Robot r2 = new Robot("batman");
-		//3. Ask the user what color they would like the robot to draw
-		JOptionPane.showInputDialog(null, "what color do you want");
-		//5. Use an if/else statement to set the pen color that the user requested
+		for (int i = 0; i < 1000000000; i++) {
 
-        //6. If the user doesn’t enter anything, choose a random color
+			// 1. Create a new Robot
+			r2.penDown();
+			// 3. Ask the user what color they would like the robot to draw
+			String WORD = JOptionPane.showInputDialog(null, "what color do you want");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (WORD.equals("blue")) {
+					r2.setPenColor(255, 187, 85);
+			}
+			if (WORD.equals("gold")) {
+				r2.setPenColor(000, 238, 255);
+			} else {
+				r2.setRandomPenColor();
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		r2.setPenWidth(10);
-		r2.setSpeed(10);	
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
 
-for (int i = 0; i < 360; i++) {
-	
-	r2.move(1);
-	r2.turn(1);	
-	}
+			// 4. Set the pen width to 10
+			r2.setPenWidth(10);
+			r2.setSpeed(10);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
 
+			for (int i1 = 0; i1 < 360; i1++) {
+
+				r2.move(1);
+				r2.turn(1);
+			}
+
+		}
 	}
 }
