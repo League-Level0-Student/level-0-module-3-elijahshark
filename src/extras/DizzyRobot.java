d95a6520279e3cb6d9bbd6b778c3fe9c84658bd1
elijahshark <league@WTS-iMac-11.attlocal.net>
2018-04-25 16:20:21 -0700

    DizzyRobot

--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -10,14 +10,16 @@ import org.jointheleague.graphical.robot.Robot;
 public class DizzyRobot {
 	static Robot robot = new Robot("batman");
 	public static void main(String[] args) {
+		for (int i = 0; i < 99999 ; i++) {
+			
 		
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
 String ans = JOptionPane.showInputDialog("how many times do you want Batman to move");
  // 1. Use the dance method to make the robot spin.
+robot.setSpeed(10);
 int LAZER_CATS_ARE_TAKING_OVER_THE_WORLD = Integer.parseInt(ans);
-
-
-
+dance(LAZER_CATS_ARE_TAKING_OVER_THE_WORLD);
+		}
 	}
 
 	static void dance(int numberOfSpins) {
